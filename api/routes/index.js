368c0a802c9663f518eefc8dad9ab21cc9b5ba03
Rodrigo Bacigalupo <rebacigalupo@gmail.com>
2021-08-01 16:10:26 -0300

    deleted api

--- api/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.use('/auth', require('./auth'));
-router.use('/favourites', require('./favourite'))
-router.use('/users', require('./users'))
-
-module.exports = router;
